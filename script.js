a542d4ced5c5e2724318a201cac27f699ba2c86d
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-17 11:06:08 +0000

    edit function called dragStart

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ for (const empty of empties) {
 }
 
 function dragStart() {
-  console.log("drag start");
+  this.className += "hold";
+  setTimeout(() => (this.className = "invisible"), 0);
 }
 
 function dragEnd() {
